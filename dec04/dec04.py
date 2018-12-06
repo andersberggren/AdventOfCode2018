bea9336800f0cbc3e27c8cebd957aee3697784c3
Anders Berggren <anders.berggren@acino.se>
2018-12-06 15:10:42 +0100

    December 4th improved with regular expressions :)

--- dec04/dec04.py
+++ dec04/dec04.py
@@ -33,15 +33,17 @@ class Event:
 
 	@staticmethod
 	def createFromString(s):
-		date = s.split()[0][1:]
-		minute = s.split()[1].split(":")[1].split("]")[0]
-		text = s.split(None, 2)[2]
+		m = re.match("\[(\d{4}-\d{2}-\d{2}) \d{2}:(\d{2})\] (.*)", s)
+		date = m.group(1)
+		minute = m.group(2)
+		text = m.group(3)
 		return Event(date, minute, text)
 
 #############
 # Functions #
 #############
 # Reads a file and returns a list of Event (in chronological order).
+# Each line in the file represents an event.
 def getEventsFromFile(fileName):
 	with open("input04") as f:
 		return [Event.createFromString(s) for s in sorted(f.readlines())]
@@ -53,20 +55,24 @@ def getGuardDataFromEvents(events):
 	currentGuard = None
 	asleepFrom = None
 	for event in events:
-		if re.match("^Guard #", event.text):
-			guardID = event.text.split()[1][1:]
+		matchGuardBeginsShift = re.match("Guard #(\d+) begins shift$", event.text)
+		if matchGuardBeginsShift:
+			guardID = matchGuardBeginsShift.group(1)
 			try:
 				currentGuard = idToGuard[guardID]
 			except KeyError:
 				currentGuard = Guard(guardID)
 				idToGuard[guardID] = currentGuard
-		elif re.match("^falls asleep", event.text):
+		elif re.match("falls asleep", event.text):
 			asleepFrom = event.minute
-		elif re.match("^wakes up", event.text):
+		elif re.match("wakes up", event.text):
 			asleepTo = event.minute
 			currentGuard.addSleepInterval(asleepFrom, asleepTo)
+		else:
+			raise RuntimeError("Unknown event: {}".format(event.text))
 	return idToGuard
 
+# Finds and prints the guard that has slept the most in total.
 def findMostSleepyGuard(guards):
 	guard = sorted(guards, key=lambda x: x.minutesAsleep, reverse=True)[0]
 	minuteMostOftenAsleep = guard.getMinuteMostOftenAsleep()
@@ -74,6 +80,7 @@ def findMostSleepyGuard(guards):
 			id=guard.id, minute=minuteMostOftenAsleep))
 	print("Answer to part 1: {}".format(guard.id * minuteMostOftenAsleep))
 
+# Finds and prints the guard that has slept the most times during a single minute of the day.
 def findGuardThatSleptTheMostDuringASingleMinute(guards):
 	guardTuples = []
 	for guard in guards:
