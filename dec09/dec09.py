fc01ad7998648b8f9c99e9fdfb57e21d822dfade
Anders Berggren <anders.berggren@acino.se>
2018-12-10 15:18:58 +0100

    December 9th refactored.

--- dec09/dec09.py
+++ dec09/dec09.py
@@ -3,70 +3,72 @@ import re
 ###########
 # Classes #
 ###########
-# A circular linked list.
+# A circular and doubly-linked list.
 class LinkedList:
 	def __init__(self):
-		self.currentItem = None
+		self.cursor = None
 		self.size = 0
 
-	# Inserts "value" before the current item.
-	# The inserted item then becomes the current item.
-	def add(self, value):
-		newItem = LinkedListItem(value)
+	# Inserts "valueToInsert" before the cursor.
+	# After insert, the cursor points to the new element.
+	def add(self, valueToInsert):
+		newElement = LinkedList.Element(valueToInsert)
 		if self.size == 0:
-			newItem.prev = newItem
-			newItem.next = newItem
+			newElement.prev = newElement
+			newElement.next = newElement
 		else:
-			itemBefore = self.currentItem.prev
-			itemBefore.next = newItem
-			self.currentItem.prev = newItem
-			newItem.next = self.currentItem
-			newItem.prev = itemBefore
-		self.currentItem = newItem
+			prevElement = self.cursor.prev
+			prevElement.next = newElement
+			self.cursor.prev = newElement
+			newElement.next = self.cursor
+			newElement.prev = prevElement
+		self.cursor = newElement
 		self.size += 1
 
-	# Removes the current item from the list, and returns it.
-	# The next item in the list becomes the current item.
+	# Removes the element at the cursor, and returns the element.
+	# After remove, the cursor points to the element after the element that was removed.
 	def remove(self):
 		if self.size == 0:
 			raise RuntimeError("Can't remove, size is 0")
-		valueToReturn = self.currentItem.value
+		valueToReturn = self.cursor.value
 		if self.size == 1:
-			self.currentItem = None
+			self.cursor = None
 		else:
-			itemBefore = self.currentItem.prev
-			itemAfter = self.currentItem.next
-			itemBefore.next = itemAfter
-			itemAfter.prev = itemBefore
-			self.currentItem = itemAfter
+			prevElement = self.cursor.prev
+			nextElement = self.cursor.next
+			prevElement.next = nextElement
+			nextElement.prev = prevElement
+			self.cursor = nextElement
 		self.size -= 1
 		return valueToReturn
 
-	# Returns the current item.
+	# Returns the element at the cursor.
 	def get(self):
-		return self.currentItem.value
+		return self.cursor.value
 
-	# Moves the "current item"-pointer forwards "steps" number of steps.
-	def forward(self, steps):
-		for i in range(steps):
-			self.currentItem = self.currentItem.next
+	# Moves the cursor "numberOfSteps" steps (can be negative).
+	def moveCursor(self, numberOfSteps):
+		while numberOfSteps != 0:
+			if numberOfSteps < 0:
+				self.cursor = self.cursor.prev
+				numberOfSteps += 1
+			elif numberOfSteps > 0:
+				self.cursor = self.cursor.next
+				numberOfSteps -= 1
 
-	# Moves the "current item"-pointer backwards "steps" number of steps.
-	def backward(self, steps):
-		for i in range(steps):
-			self.currentItem = self.currentItem.prev
-
-class LinkedListItem:
-	def __init__(self, value):
-		self.prev = None
-		self.next = None
-		self.value = value
+	# An element in the list, with references to the previous and next element in the list.
+	class Element:
+		def __init__(self, value):
+			self.value = value
+			self.prev = None
+			self.next = None
 
 class MarbleGame:
-	def __init__(self, numberOfPlayers):
+	def __init__(self, numberOfPlayers, valueOfFinalMarble):
 		self.marbles = LinkedList()
 		self.marbles.add(0)
 		self.valueOfNextMarble = 1
+		self.valueOfFinalMarble = valueOfFinalMarble
 		self.numberOfPlayers = numberOfPlayers
 		self.currentPlayerIndex = 0
 		# Dict, from player index (starting at 0), to the players score
@@ -74,26 +76,25 @@ class MarbleGame:
 		for i in range(numberOfPlayers):
 			self.playerToScore[i] = 0
 
-	def playNextMarble(self):
-		if self.valueOfNextMarble % 23 == 0:
-			# Special case for multiples of 23
-			self.marbles.backward(7)
-			self.playerToScore[self.currentPlayerIndex] += self.valueOfNextMarble
-			self.playerToScore[self.currentPlayerIndex] += self.marbles.get()
-			self.marbles.remove()
-		else:
-			self.marbles.forward(2)
-			self.marbles.add(self.valueOfNextMarble)
-		self.valueOfNextMarble += 1
-		self.currentPlayerIndex = (self.currentPlayerIndex+1) % self.numberOfPlayers
-
-	def playGame(self, valueOfFinalMarble):
-		while self.valueOfNextMarble <= valueOfFinalMarble:
+	def playGame(self):
+		while self.valueOfNextMarble <= self.valueOfFinalMarble:
 			self.playNextMarble()
 		(playerIndex, score) = sorted(self.playerToScore.items(), key=lambda item: item[1], reverse=True)[0]
 		# Add 1 to playerIndex.
 		# Internally, index starts at 0, but the answer expects index to start at 1
 		return (playerIndex+1, score)
+
+	def playNextMarble(self):
+		if self.valueOfNextMarble % 23 == 0:
+			# Special case for multiples of 23
+			self.marbles.moveCursor(-7)
+			self.playerToScore[self.currentPlayerIndex] += self.valueOfNextMarble
+			self.playerToScore[self.currentPlayerIndex] += self.marbles.remove()
+		else:
+			self.marbles.moveCursor(2)
+			self.marbles.add(self.valueOfNextMarble)
+		self.valueOfNextMarble += 1
+		self.currentPlayerIndex = (self.currentPlayerIndex+1) % self.numberOfPlayers
 
 #############
 # Functions #
@@ -118,11 +119,9 @@ def getGameParametersFromFile(fileName):
 (numberOfPlayers, valueOfFinalMarble) = getGameParametersFromFile("input09")
 
 # Part 1
-marbleGame = MarbleGame(numberOfPlayers)
-(winningPlayer, score) = marbleGame.playGame(valueOfFinalMarble)
+(winningPlayer, score) = MarbleGame(numberOfPlayers, valueOfFinalMarble).playGame()
 print("Part 1: Player {player} wins with {score} points".format(player=winningPlayer, score=score))
 
 # Part 2
-marbleGame = MarbleGame(numberOfPlayers)
-(winningPlayer, score) = marbleGame.playGame(valueOfFinalMarble*100)
+(winningPlayer, score) = MarbleGame(numberOfPlayers, valueOfFinalMarble*100).playGame()
 print("Part 2: Player {player} wins with {score} points".format(player=winningPlayer, score=score))
