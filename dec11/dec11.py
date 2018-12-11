2b18a3030beb9cd4aa5738dd3d576ae51b4bfb1f
Anders Berggren <anders.berggren@acino.se>
2018-12-11 10:08:48 +0100

    December 11th, part 2, brute force solution.
    Managed to find the correct answer at size 19x19. Did not calculate up
    to size 300x300, because execution was slow. Calculated up to 50x50.

--- dec11/dec11.py
+++ dec11/dec11.py
@@ -1,36 +1,56 @@
+###########
+# Classes #
+###########
+class Grid:
+	def __init__(self, width, height, serialNumber):
+		self.width = width
+		self.height = height
+		self.serialNumber = serialNumber
+		self.coordToPowerLevel = {}
+		for x in range(1, width+1):
+			for y in range(1, height+1):
+				self.coordToPowerLevel[(x,y)] = self.calcPowerLevel(x, y)
+
+	def calcPowerLevel(self, x, y):
+		rackID = x + 10
+		powerLevel = ((rackID*y) + self.serialNumber) * rackID
+		powerLevel = (powerLevel//100) % 10 - 5
+		return powerLevel
+
+	def calcPowerLevelAggregated(self, topLeftX, topLeftY, size):
+		xRange = range(topLeftX, topLeftX+size)
+		yRange = range(topLeftY, topLeftY+size)
+		return sum([self.coordToPowerLevel[(x,y)] for x in xRange for y in yRange])
+
+	# Returns (coordinate, totalPower).
+	def getLargestTotalPower(self, size):
+		coordToTotalPower = {}
+		for x in range(1, grid.width+2-size):
+			for y in range(1, grid.height-+2-size):
+				coordToTotalPower[(x,y)] = self.calcPowerLevelAggregated(x, y, size)
+		return sorted(coordToTotalPower.items(), key=lambda x: x[1], reverse=True)[0]
+
 #############
 # Functions #
 #############
-def calcPowerLevel(x, y, serialNumber):
-	rackID = x + 10
-	powerLevel = rackID * y
-	powerLevel += serialNumber
-	powerLevel *= rackID
-	powerLevel = (powerLevel//100) % 10
-	powerLevel -= 5
-	return powerLevel
-
-def calcPowerLevel3x3(x, y, coordToPowerLevel):
-	return sum([coordToPowerLevel[(xIter,yIter)] for xIter in range(x, x+3) for yIter in range(y, y+3)])
 
 ########
 # Main #
 ########
-serialNumber = 1308
-minX = 1
-maxX = 300
-minY = 1
-maxY = 300
+grid = Grid(300, 300, 1308)
 
-coordToPowerLevel = {}
-for x in range(minX, maxX+1):
-	for y in range(minY, maxY+1):
-		coordToPowerLevel[(x,y)] = calcPowerLevel(x, y, serialNumber)
-
-coordToPowerLevel3x3 = {}
-for x in range(minX, maxX-1):
-	for y in range(minY, maxY-1):
-		coordToPowerLevel3x3[(x,y)] = calcPowerLevel3x3(x, y, coordToPowerLevel)
-
-(coord, powerLevel) = sorted(coordToPowerLevel3x3.items(), key=lambda item: item[1], reverse=True)[0]
+# Part 1
+(coord, powerLevel) = grid.getLargestTotalPower(3)
 print("3x3 square at {coord} has highest power level: {pl}".format(coord=coord, pl=powerLevel))
+
+# Part 2
+# Brute force solution. Execution time increases as size of square increases.
+# At square size ~50x50, execution is awfully slow.
+# The largest total power at this point was a 19x19 square.
+# I submitted that, and it turned out to be right, but I didn't know that before I submitted it,
+# because all square sizes hadn't been evaluated.
+# Performance can be improved by re-using values calculated for previous smaller squares.
+for i in range(1, grid.width+1):
+	(coord, powerLevel) = grid.getLargestTotalPower(i)
+	print("{i}x{i} square at {coord} has highest power level: {pl}".format(
+			i=i, coord=coord, pl=powerLevel), flush=True)
