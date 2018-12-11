b1ecdb087ac5c96c6cddb7a2a31bf73ff02a7a9c
Anders Berggren <anders.berggren@acino.se>
2018-12-11 14:22:11 +0100

    December 11th, performance improvements.

--- dec11/dec11.py
+++ dec11/dec11.py
@@ -2,55 +2,90 @@
 # Classes #
 ###########
 class Grid:
-	def __init__(self, width, height, serialNumber):
-		self.width = width
-		self.height = height
+	def __init__(self, size, serialNumber):
+		self.size = size
 		self.serialNumber = serialNumber
-		self.coordToPowerLevel = {}
-		for x in range(1, width+1):
-			for y in range(1, height+1):
-				self.coordToPowerLevel[(x,y)] = self.calcPowerLevel(x, y)
+		# Dict, key is cell coordinate (x,y), value is power level.
+		self.cellToPowerLevel = {}
+		for x in range(1, size+1):
+			for y in range(1, size+1):
+				self.cellToPowerLevel[(x,y)] = self.calcPowerLevelForSingleCell(x, y)
+		# Dict, key is top-left coordinate and size (x,y,size), value is total power.
+		self.squareToTotalPower = {}
 
-	def calcPowerLevel(self, x, y):
+	# Returns the power level for the cell at (x,y).
+	def calcPowerLevelForSingleCell(self, x, y):
 		rackID = x + 10
 		powerLevel = ((rackID*y) + self.serialNumber) * rackID
 		powerLevel = (powerLevel//100) % 10 - 5
 		return powerLevel
 
-	def calcPowerLevelAggregated(self, topLeftX, topLeftY, size):
-		xRange = range(topLeftX, topLeftX+size)
-		yRange = range(topLeftY, topLeftY+size)
-		return sum([self.coordToPowerLevel[(x,y)] for x in xRange for y in yRange])
+	# Returns the total power of the square with size squareSize and
+	# top-left coordinate (topLeftX, topLeftY).
+	def calcTotalPowerInSquare(self, topLeftX, topLeftY, squareSize):
+		totalPower = 0
+		if squareSize == 1:
+			totalPower = self.cellToPowerLevel[(topLeftX,topLeftY)]
+			self.squareToTotalPower[(topLeftX,topLeftY,squareSize)] = totalPower
+			return totalPower
 
-	# Returns (coordinate, totalPower).
-	def getLargestTotalPower(self, size):
-		coordToTotalPower = {}
-		for x in range(1, grid.width+2-size):
-			for y in range(1, grid.height-+2-size):
-				coordToTotalPower[(x,y)] = self.calcPowerLevelAggregated(x, y, size)
-		return sorted(coordToTotalPower.items(), key=lambda x: x[1], reverse=True)[0]
+		# Which strategy requires the fewest lookups?
+		divisor = getLargestDivisorSmallerThanSelf(squareSize)
+		defaultStrategy = squareSize * 2
+		alternativeStrategy = (squareSize//divisor) ** 2
+		if defaultStrategy <= alternativeStrategy:
+			# Default strategy:
+			# Get total power of square that is 1 size smaller,
+			# then add the power levels of the cells along the right and bottom edge.
+			totalPower = self.squareToTotalPower[(topLeftX, topLeftY, squareSize-1)]
+			for x in range(topLeftX, topLeftX+squareSize):
+				y = topLeftY + squareSize - 1
+				totalPower += self.cellToPowerLevel[(x,y)]
+			for y in range(topLeftY, topLeftY+squareSize-1):
+				x = topLeftX + squareSize - 1
+				totalPower += self.cellToPowerLevel[(x,y)]
+		else:
+			# Alternative strategy:
+			# Get total power from smaller sub-squares that have already been calculated.
+			for x in range(topLeftX, topLeftX+squareSize, divisor):
+				for y in range(topLeftY, topLeftY+squareSize, divisor):
+					totalPower += self.squareToTotalPower[(x,y,divisor)]
+		self.squareToTotalPower[(topLeftX,topLeftY,squareSize)] = totalPower
+		return totalPower
+
+	# Returns ((x,y), totalPower).
+	def getLargestTotalPower(self, squareSize):
+		# Make sure we have calculated and cached the total power for square sizes <= squareSize.
+		for i in range(1, squareSize+1):
+			if (1,1,i) not in self.squareToTotalPower:
+				for x in range(1, self.size+2-i):
+					for y in range(1, self.size+2-i):
+						self.calcTotalPowerInSquare(x, y, i)
+		items = [x for x in self.squareToTotalPower.items() if x[0][2] == squareSize]
+		((x, y, size), totalPower) = sorted(items, key=lambda item: item[1], reverse=True)[0]
+		return ((x,y), totalPower)
 
 #############
 # Functions #
 #############
+def getLargestDivisorSmallerThanSelf(x):
+	largestDivisor = 1
+	for divisor in range(1, x):
+		if x % divisor == 0:
+			largestDivisor = divisor
+	return largestDivisor
 
 ########
 # Main #
 ########
-grid = Grid(300, 300, 1308)
+grid = Grid(300, 1308)
 
-# Part 1
-(coord, powerLevel) = grid.getLargestTotalPower(3)
-print("3x3 square at {coord} has highest power level: {pl}".format(coord=coord, pl=powerLevel))
-
-# Part 2
-# Brute force solution. Execution time increases as size of square increases.
-# At square size ~50x50, execution is awfully slow.
-# The largest total power at this point was a 19x19 square.
-# I submitted that, and it turned out to be right, but I didn't know that before I submitted it,
-# because all square sizes hadn't been evaluated.
-# Performance can be improved by re-using values calculated for previous smaller squares.
-for i in range(1, grid.width+1):
-	(coord, powerLevel) = grid.getLargestTotalPower(i)
-	print("{i}x{i} square at {coord} has highest power level: {pl}".format(
-			i=i, coord=coord, pl=powerLevel), flush=True)
+bestSoFar = None
+for i in range(1, grid.size+1):
+	(coord, totalPower) = grid.getLargestTotalPower(i)
+	print("Among all {i}x{i} squares, the one at {coord} has the highest total power: {tp}".format(
+			i=i, coord=coord, tp=totalPower), flush=True)
+	if bestSoFar is None or totalPower > bestSoFar[2]:
+		bestSoFar = (coord, i, totalPower)
+	print("So far, the {size}x{size} square at {coord} has the highest total power: {tp}".format(
+			size=bestSoFar[1], coord=bestSoFar[0], tp=bestSoFar[2]), flush=True)
