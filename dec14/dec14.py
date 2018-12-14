2259251e336403a3ab848aa7d08f910c8a6537b9
Anders Berggren <anders.berggren@acino.se>
2018-12-14 14:06:40 +0100

    December 14th improved.

--- dec14/dec14.py
+++ dec14/dec14.py
@@ -1,53 +1,57 @@
+#########
+# Class #
+#########
+class ChocolateLaboratory:
+	def __init__(self):
+		self.recipeScores = [3, 7]
+		self.elves = [0, 1]
+
+	# Appends new recipe scores (one or two) at the end of recipeScores,
+	# and updates which recipes the elves are at.
+	# Returns the number of recipe scores added.
+	def update(self):
+		scoreSumAsString = str(sum([self.recipeScores[elf] for elf in self.elves]))
+		self.recipeScores.extend([int(digitAsString) for digitAsString in scoreSumAsString])
+		self.elves = [(elf+self.recipeScores[elf]+1) % len(self.recipeScores) for elf in self.elves]
+		return len(scoreSumAsString)
+
 #############
 # Functions #
 #############
 # Arguments:
-#   recipeScores     Recipe scores (list of integers).
-#   elves            Index in recipeScores where an elf is at (a list of integers).
+#   lab              The chocolate laboratory
 #   numberOfRecipes  The number of recipe scores to generate, before the answer.
-#   answerSize       The number of recipe scores, after numberOfRecipes, that will make up the answer.
+#   answerSize       The number of recipe scores, after numberOfRecipes, for the answer.
 #
-# Returns a string with the answerSize digits that follows after generating numberOfRecipes recipes.
-def part1(recipeScores, elves, numberOfRecipes, answerSize):
-	recipeScores = recipeScores.copy()
-	elves = elves.copy()
-	while len(recipeScores) < numberOfRecipes+answerSize:
-		appendScoresForNewRecipesAndMoveElves(recipeScores, elves)
-	return "".join([str(d) for d in recipeScores[numberOfRecipes:numberOfRecipes+answerSize]])
+# Returns a string with the answerSize digits that follows immediately after
+# generating numberOfRecipes recipes.
+def part1(lab, numberOfRecipes, answerSize):
+	while len(lab.recipeScores) < numberOfRecipes+answerSize:
+		lab.update()
+	answer = "".join([str(d) for d in lab.recipeScores[numberOfRecipes:numberOfRecipes+answerSize]])
+	print("Part 1. Digit sequence after {n} recipes: {a}".format(n=numberOfRecipes, a=answer))
 
 # Arguments:
-#   recipeScores   A list of integers, representing scores for recipes
-#   elves          A list of integers, representing the index in recipeScores where an elf is at.
+#   lab            The chocolate laboratory
 #   digitSequence  A list of integers. This is the sequence of digits we look for in recipeScores.
 #
 # Returns the number of recipe scores that need to be generated,
-# before recipe scores occur according to digitSequence.
-def part2(recipeScores, elves, digitSequence):
+# before the digit sequence in digitSequence occurs in the recipe scores.
+def part2(lab, digitSequence):
 	while True:
-		numberOfNewScores = appendScoresForNewRecipesAndMoveElves(recipeScores, elves)
+		numberOfNewScores = lab.update()
 		for i in range(numberOfNewScores):
-			offset = len(recipeScores) - len(digitSequence) - i
-			if matchSubList(recipeScores, digitSequence, offset):
-				return offset
-			if len(recipeScores) % 100000 == 0:
-				print("{: >11,} recipes created".format(len(recipeScores)), flush=True)
+			offset = len(lab.recipeScores) - len(digitSequence) - (numberOfNewScores-1) + i
+			if offset >= 0 and matchSubList(lab.recipeScores, digitSequence, offset):
+				print("Part 2. Digit sequence occurs after {answer} recipes".format(answer=offset))
+				return
+		if (len(lab.recipeScores) % 1000000) - numberOfNewScores < 0:
+			print("{: >11,} recipes created".format(len(lab.recipeScores)), flush=True)
 
-# Appends new recipe scores (one or two) at the end of recipeScores,
-# and moves the elves to new recipes.
-# Returns the number of recipe scores added.
-def appendScoresForNewRecipesAndMoveElves(recipeScores, elves):
-	scoreSumAsString = str(sum([recipeScores[elf] for elf in elves]))
-	for digitAsString in scoreSumAsString:
-		recipeScores.append(int(digitAsString))
-	for i in range(len(elves)):
-		elves[i] = (elves[i] + recipeScores[elves[i]] + 1) % len(recipeScores)
-	return len(scoreSumAsString)
-
-# Returns True iff for each element in subList we can find an equal element in fullList.
-# The entire list subList will be compared against fullList, starting at index "offset" in fullList.
+# Returns True iff for each element in subList we can find an equal element at the corresponding
+# index in fullList. The entire list subList will be compared against fullList, starting at index
+# "offset" in fullList.
 def matchSubList(fullList, subList, offset):
-	if len(fullList) < len(subList) or offset < 0:
-		return False
 	for i in range(len(subList)):
 		if fullList[offset+i] != subList[i]:
 			return False
@@ -56,10 +60,8 @@ def matchSubList(fullList, subList, offset):
 ########
 # Main #
 ########
-recipeScores = [3, 7]
-elves = [0, 1]
 puzzleInput = "440231"
 answerSizePart1 = 10
 
-print("Part 1: {}".format(part1(recipeScores, elves, int(puzzleInput), answerSizePart1)))
-print("Part 2: {}".format(part2(recipeScores, elves, [int(x) for x in puzzleInput])))
+part1(ChocolateLaboratory(), int(puzzleInput), answerSizePart1)
+part2(ChocolateLaboratory(), [int(x) for x in puzzleInput])
