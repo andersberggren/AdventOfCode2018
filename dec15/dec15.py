63278d98f9b2f2252517edd20b6a2ebc0bb7081c
Anders Berggren <anders.berggren@acino.se>
2018-12-19 14:43:17 +0100

    December 15th, part 1.

--- dec15/dec15.py
+++ dec15/dec15.py
@@ -9,7 +9,8 @@ class Creature:
 	def __init__(self, position, type):
 		self.position = position
 		self.type = type
-		self.hitpoints = 200
+		self.attackPower = 3
+		self.hitPoints = 200
 
 class World:
 	def __init__(self):
@@ -22,9 +23,18 @@ class World:
 	def addCreature(self, creature):
 		self.positionToCreature[creature.position] = creature
 
+	def moveCreature(self, creature, newPosition):
+		del self.positionToCreature[creature.position]
+		creature.position = newPosition
+		self.positionToCreature[creature.position] = creature
+
+	def attackCreature(self, attacker, defender):
+		defender.hitPoints -= attacker.attackPower
+		if defender.hitPoints <= 0:
+			del self.positionToCreature[defender.position]
+
 	def getCreaturesSortedByExecutionOrder(self):
-		sortKey = lambda x: (x.position[1], x.position[0])
-		return sorted(self.positionToCreature.values(), key=sortKey)
+		return sorted(self.positionToCreature.values(), key=lambda x: positionSortKey(x.position))
 
 	def getEnemies(self, friendType):
 		return [x for x in self.positionToCreature.values() if x.type != friendType]
@@ -32,10 +42,10 @@ class World:
 	def getAdjacentSquares(self, position):
 		(x,y) = position
 		positions = [(x,y+1), (x,y-1), (x+1,y), (x-1,y)]
-		return [p for p in positions if p in self.allPositions]
+		return set([p for p in positions if p in self.allPositions])
 
 	def getAdjacentEmptySquares(self, position):
-		return [p for p in self.getAdjacentSquares(position) if self.isEmpty(p)]
+		return set([p for p in self.getAdjacentSquares(position) if self.isEmpty(p)])
 
 	def getDistance(self, positionFrom, positionTo):
 		visitedSquares = set([positionFrom])
@@ -63,7 +73,7 @@ class World:
 
 	def isReachable(self, positionFrom, positionTo):
 		evaluated = set()
-		leftToEvaluate = set(self.getAdjacentEmptySquares(positionFrom))
+		leftToEvaluate = set([positionFrom])
 		while len(leftToEvaluate) > 0:
 			position = leftToEvaluate.pop()
 			if position == positionTo:
@@ -110,26 +120,52 @@ def readWorldFromFile(fileName):
 
 def doOneRoundOfActions(world):
 	for creature in world.getCreaturesSortedByExecutionOrder():
-		# Move:
-		# - If adjacent to enemy, do nothing.
-		# - If there exists a reachable square adjacent to an enemy,
-		#   move one step towards the closest reachable square.
-		if not world.existsAdjacentEnemy(creature):
-			enemies = world.getEnemies(creature.type)
-			targetSquares = set()
-			for enemy in enemies:
-				targetSquares |= world.getAdjacentEmptySquares(enemy.position)
-			targetSquares = set([x for x in targetSquares if world.isReachable(creature.position, x)])
-			distancesAndPositions = [(x, world.getDistance(creature.position, x)) for x in targetSquares]
-			minDistance = min([x[1] for x in distancesAndPositions])
-			closestSquares = [x[0] for x in distancesAndPositions if x[1] == minDistance]
-			sortKey = lambda x: (x[1], x[0])
-			if closestSquares:
-				closestSquare = sorted(closestSquares, key=sortKey)[0]
-				# TODO Find which square to move to
+		if creature.hitPoints > 0:
+			creatureMove(world, creature)
+			creatureAttack(world, creature)
 
-		# TODO Attack
-		# ...
+# Moves creatures, according to:
+# 1. If adjacent to enemy, do nothing.
+# 2. If there exists a reachable square adjacent to an enemy,
+#    move one step towards the closest reachable square.
+def creatureMove(world, creature):
+	if world.existsAdjacentEnemy(creature):
+		return
+	enemies = world.getEnemies(creature.type)
+	candidateSquares = set()
+	for enemy in enemies:
+		candidateSquares |= world.getAdjacentEmptySquares(enemy.position)
+	candidateSquares = set([x for x in candidateSquares if world.isReachable(creature.position, x)])
+	if not candidateSquares:
+		return
+	distancesAndPositions = [(x, world.getDistance(creature.position, x)) for x in candidateSquares]
+	minDistance = min([x[1] for x in distancesAndPositions])
+	candidateSquares = [x[0] for x in distancesAndPositions if x[1] == minDistance]
+	targetSquare = sorted(candidateSquares, key=positionSortKey)[0]
+	
+	adjacentSquares = world.getAdjacentEmptySquares(creature.position)
+	distancesAndPositions = [(x, world.getDistance(x, targetSquare)) for x in adjacentSquares if world.isReachable(x, targetSquare)]
+	minDistance = min([x[1] for x in distancesAndPositions])
+	candidateSquares = [x[0] for x in distancesAndPositions if x[1] == minDistance]
+	moveTo = sorted(candidateSquares, key=positionSortKey)[0]
+	world.moveCreature(creature, moveTo)
+
+def creatureAttack(world, creature):
+	adjacentSquares = world.getAdjacentSquares(creature.position)
+	adjacentEnemies = [x for x in world.getEnemies(creature.type) if x.position in adjacentSquares]
+	if not adjacentEnemies:
+		return
+	# Sort by hit points
+	adjacentEnemies = sorted(adjacentEnemies, key=lambda x: x.hitPoints)
+	adjacentEnemies = [x for x in adjacentEnemies if x.hitPoints == adjacentEnemies[0].hitPoints]
+	# Sort by reading order
+	targetEnemy = sorted(adjacentEnemies, key=lambda x: positionSortKey(x.position))[0]
+	world.attackCreature(creature, targetEnemy)
+
+# Sort key for positions (x,y), to be sorted in reading order.
+# Pass this function as "key" argument to sorted(listToSort, key=positionSortKey)
+def positionSortKey(position):
+	return (position[1], position[0])
 
 # Temp debug
 def printWorld(world):
@@ -142,10 +178,7 @@ def printWorld(world):
 			if position in world.positionToCreature:
 				symbol = world.positionToCreature[position].type[0]
 			elif position in world.allPositions:
-				if world.isReachable((1,11), position):
-					symbol = "R"
-				else:
-					symbol = "."
+				symbol = "."
 			print(symbol, end="")
 		print()
 
@@ -154,5 +187,14 @@ def printWorld(world):
 ########
 if __name__ == "__main__":
 	world = readWorldFromFile("input15")
-	print()
-	printWorld(world)
+	rounds = 0
+	while True:
+		doOneRoundOfActions(world)
+		if world.isCombatOver():
+			hitPointsLeft = sum([x.hitPoints for x in world.positionToCreature.values()])
+			print("Outcome: {}".format(rounds * hitPointsLeft))
+			break
+		else:
+			rounds += 1
+			printWorld(world)
+			print("Rounds completed: {}".format(rounds), flush=True)
