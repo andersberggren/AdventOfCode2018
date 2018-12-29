64a10ee068dbeb4e912583299da5aeda0cb7f059
Anders Berggren <anders.e.berggren@gmail.com>
2018-12-29 18:33:09 +0100

    December 23rd, improved.

--- aoclib/filereader.py
+++ aoclib/filereader.py
@@ -1,3 +1,7 @@
 def getFileAsSingleString(fileName):
 	with open(fileName) as f:
 		return f.read()
+
+def getFileAsListOfString(fileName):
+	with open(fileName) as f:
+		return f.readlines()
--- dec25/dec25.py
+++ dec25/dec25.py
@@ -1,11 +1,12 @@
 from aoclib.distance import getManhattanDistance4
+from aoclib.filereader import getFileAsListOfString
 
 ###########
 # Classes #
 ###########
 class Constellation:
-	def __init__(self):
-		self.members = set()
+	def __init__(self, point):
+		self.members = {point}
 
 	def addPoint(self, point):
 		self.members.add(point)
@@ -14,38 +15,35 @@ class Constellation:
 # Functions #
 #############
 def getPointsFromFile(fileName):
-	with open(fileName) as f:
-		return set([getPointFromString(x) for x in f.readlines()])
+	return set([getPointFromString(x) for x in getFileAsListOfString(fileName)])
 
 def getPointFromString(s):
 	return tuple([int(x) for x in s.strip().split(",")])
+
+def tryToJoinConstellations(pointToConstellation):
+	progress = True
+	while progress:
+		progress = False
+		points = list(pointToConstellation.keys())
+		for i in range(len(points)):
+			pointA = points[i]
+			constellationA = pointToConstellation[pointA]
+			for pointB in points[i+1:]:
+				constellationB = pointToConstellation[pointB]
+				if constellationA != constellationB and getManhattanDistance4(pointA, pointB) <= 3:
+					joinConstellations(constellationA, constellationB, pointToConstellation)
+					progress = True
+	print("Part 1: Number of constellations: {}".format(len(set(pointToConstellation.values()))))
 
 def joinConstellations(constellationA, constellationB, pointToConstellation):
 	for point in constellationB.members:
 		constellationA.addPoint(point)
 		pointToConstellation[point] = constellationA
+	constellationB.members = []
 
 ########
 # Main #
 ########
-points = getPointsFromFile("input25.txt")
-pointToConstellation = {}
-for point in points:
-	constellation = Constellation()
-	constellation.addPoint(point)
-	pointToConstellation[point] = constellation
-
-progress = True
-while progress:
-	progress = False
-	for pointA in set(points):
-		constellationA = pointToConstellation[pointA]
-		for pointB in set(points):
-			constellationB = pointToConstellation[pointB]
-			if constellationA != constellationB and getManhattanDistance4(pointA, pointB) <= 3:
-				joinConstellations(constellationA, constellationB, pointToConstellation)
-				progress = True
-				numberOfConstellations = len(set(pointToConstellation.values()))
-				print("Joined two constellations. Remaining constellations: {}".format(numberOfConstellations))
-numberOfConstellations = len(set(pointToConstellation.values()))
-print("No more joins can be done. Remaining constellations: {}".format(numberOfConstellations))
+if __name__ == "__main__":
+	pointToConstellation = {p: Constellation(p) for p in getPointsFromFile("input25.txt")}
+	tryToJoinConstellations(pointToConstellation)
