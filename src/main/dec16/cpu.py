326edcd61d80986ff49f967279d242721435de68
Anders Berggren <anders.berggren@acino.se>
2019-01-04 12:34:40 +0100

    December 16th, refactored.

--- src/main/dec16/cpu.py
+++ src/main/dec16/cpu.py
@@ -1,3 +1,63 @@
+class CPU:
+	allOperations = set()
+	
+	def __init__(self):
+		self.registers = [0, 0, 0, 0]
+		# Dict. Key is opcode (int). Value is a set of Operation that the opcode could represent.
+		self.opcodeToMatchingOperations = {}
+		# Dict. Key is opcode (int). Value is corresponding Operation.
+		self.opcodeToOperation = {}
+	
+	def executeInstructions(self, instructions):
+		if not self.areAllOpcodesSolved():
+			raise RuntimeError("All opcodes aren't solved")
+		for instruction in instructions:
+			opcode = instruction[0]
+			operation = self.opcodeToOperation[opcode]
+			operation.execInstruction(self.registers, instruction)
+	
+	def getNumberOfMatchingOperations(self, instructionExample):
+		opcode = instructionExample.instruction[0]
+		if opcode not in self.opcodeToMatchingOperations:
+			# First time encountering this opcode. Initially, all operations could match.
+			self.opcodeToMatchingOperations[opcode] = set(CPU.allOperations)
+		nMatches = 0
+		for operation in CPU.allOperations:
+			tempRegisters = list(instructionExample.registersBefore)
+			operation.execInstruction(tempRegisters, instructionExample.instruction)
+			if tempRegisters == instructionExample.registersAfter:
+				nMatches += 1
+			else:
+				try:
+					self.opcodeToMatchingOperations[opcode].remove(operation)
+				except KeyError:
+					pass
+		self.trySolveOpcodes()
+		return nMatches
+	
+	def trySolveOpcodes(self):
+		self.updateSolvedOpcodes()
+		solvedOpcodes = set(self.opcodeToOperation.keys())
+		solvedOperations = set(self.opcodeToOperation.values())
+		progress = False
+		for opcode in [x for x in self.opcodeToMatchingOperations if x not in solvedOpcodes]:
+			if len(self.opcodeToMatchingOperations[opcode] & solvedOperations) > 0:
+				self.opcodeToMatchingOperations[opcode] -= solvedOperations
+				progress = True
+		self.updateSolvedOpcodes()
+		if progress and not self.areAllOpcodesSolved():
+			self.trySolveOpcodes()
+	
+	def updateSolvedOpcodes(self):
+		self.opcodeToOperation = {
+			opcode: next(iter(operations))
+			for (opcode, operations) in self.opcodeToMatchingOperations.items()
+			if len(operations) == 1
+		}
+	
+	def areAllOpcodesSolved(self):
+		return len(self.opcodeToOperation) == len(self.opcodeToMatchingOperations)
+
 class Operation:
 	def __init__(self, calcValue):
 		self.calcValue = calcValue
@@ -82,7 +142,7 @@ def calcValueEqrr(registerA, registerB, valueA, valueB):
 	else:
 		return 0
 
-allOperations = [
+CPU.allOperations = {
 	Operation(calcValueAddr), Operation(calcValueAddi),
 	Operation(calcValueMulr), Operation(calcValueMuli),
 	Operation(calcValueBanr), Operation(calcValueBani),
@@ -90,4 +150,4 @@ allOperations = [
 	Operation(calcValueSetr), Operation(calcValueSeti),
 	Operation(calcValueGtir), Operation(calcValueGtri), Operation(calcValueGtrr),
 	Operation(calcValueEqir), Operation(calcValueEqri), Operation(calcValueEqrr)
-]
+}
